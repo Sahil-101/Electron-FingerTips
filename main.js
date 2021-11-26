cd42c7798293c3e645db703df5c6a4c774bef5ab
Sahil-101 <sahilflash123@gmail.com>
2021-11-26 18:46:53 +0530

    Working basic prototype

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //app module controls your app's event lifecycle
 //BrowserWindow for the window application
 const { app, BrowserWindow } = require("electron");
+require("dotenv").config();
 
 //function to call to create BroserWindow
 const createWindow = () => {
@@ -18,6 +19,23 @@ app.whenReady().then(() => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
+
+const { exec } = require("child_process");
+
+exec(
+  `discord && npm --prefix ${process.env.PATH_TO_FINGERTIPS} run start`,
+  (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  }
+);
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
