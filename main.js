87bc3a6ddf921c700e48590756b6d8f6a80a8f25
Sahil-101 <sahilflash123@gmail.com>
2021-11-26 16:14:01 +0530

    basic application

---
+++ main.js
@@ -0,0 +1,24 @@
+//app module controls your app's event lifecycle
+//BrowserWindow for the window application
+const { app, BrowserWindow } = require("electron");
+
+//function to call to create BroserWindow
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
